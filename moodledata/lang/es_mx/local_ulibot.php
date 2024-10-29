fcd2b33cc600785e1784eddf48561b29fb6197b0
Sebastian Escobar Marin <sebastianescobar17@hotmail.com>
2024-10-29 11:52:32 -0500

    feat: added the project and docker

---
+++ moodledata/lang/es_mx/local_ulibot.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'local_ulibot', language 'es_mx', version '4.4'.
+ *
+ * @package     local_ulibot
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['pluginname'] = 'Cliente Ulibot';
+$string['settingstitle'] = 'Configuraciones de Ulibot';
+$string['shortdescription'] = 'Este plugin fue desarrollado para incrustar el widget ULIBOT en Moodle.';
+$string['tokenulibot'] = 'Yoken Ulibot';
+$string['tokenulibotdesc'] = 'Ingrese aquí el token de conexión al servicio Ulibot';
+$string['ulibot:courseview'] = 'Ver Ulibot en curso';
+$string['ulibot:systemview'] = 'Ver Ulibot  en sistema';
+$string['ulibotheading'] = 'Parámetros de conexión de Ulibot';
+$string['ulibotinfo'] = 'Mostrar Ulibot  en Moodle';
+$string['urlulibot'] = 'URL de Ulibot';
+$string['urlulibotdesc'] = 'Ingrese aquí  la URL de la conexión al servicio Ulibot';
