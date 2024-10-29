26469e8c2d687ea1156b5256c19a39373f326cb0
Sebastian Escobar Marin <sebastianescobar17@hotmail.com>
2024-10-29 12:11:25 -0500

    Revert "Revert "feat: added the project and docker""
    This reverts commit 2ba52c9216a4ec7362772ecfead2f19be87a780e.

---
+++ moodledata/lang/es_mx/block_timezoneclock.php
@@ -0,0 +1,56 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_timezoneclock', language 'es_mx', version '4.4'.
+ *
+ * @package     block_timezoneclock
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['addtimezone'] = 'Añadir {no} zona(s) horaria(s) a formato';
+$string['clocktype'] = 'Tipo de reloj';
+$string['clocktype_desc'] = 'Tipo predeterminado de reloj, por ejemplo digital o análogo';
+$string['configtitle'] = 'Título del bloque';
+$string['configtitle_placeholder'] = 'Predeterminado: Reloj Zona horaria';
+$string['convert'] = 'Convertir';
+$string['convertedtimes'] = 'Horas convertidas';
+$string['datestamp'] = 'Seleccionar hora';
+$string['entervalidtimestamp'] = 'Escribir sello de tiempo UNIX válido';
+$string['fromtimezone'] = 'Desde zona horaria';
+$string['pagetitle:converter'] = 'Convertidor de Zona horaria';
+$string['pluginname'] = 'Reloj de Zona horaria';
+$string['preferred_timezones'] = 'Zonas horarias preferidas';
+$string['privacy:metadata'] = 'El reloj de zona horaria solamente muestra datos almacenados en otros lugares.';
+$string['timestamp'] = 'Sello UNIX';
+$string['timestamp_placeholder'] = 'Ingresar unixstamp';
+$string['timestampnotentered'] = 'Ya sea que escriba el sello UNIX, O BIEN, la hora seleccionada';
+$string['timezoneclock:addinstance'] = 'Añadir un bloque de reloj zona horaria';
+$string['timezoneclock:myaddinstance'] = 'Añadir un bloque de reloj zona horaria al Tablero';
+$string['timezoneconveter'] = 'Convertidor';
+$string['timezones'] = 'Zonas horarias';
+$string['timezoneuser'] = 'Su zona horaria ({$a})';
+$string['title'] = 'Reloj de zona horaria';
+$string['toggletimeinput'] = 'Alternar unixstamp';
+$string['toggletimeinput_checkicons'] = '<i class="fa-regular fa-square-check"></i><i class="fa-solid fa-square-check"></i>';
+$string['totimezones'] = 'A Zonas horarias';
+$string['typeanalog'] = 'Análogo';
+$string['typedigital'] = 'Digital';
+$string['yourtimezone'] = 'Su zona horaria';
---
+++ moodledata/lang/es_mx/tool_selfsignuphardlifecycle.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_selfsignuphardlifecycle', language 'es_mx', version '4.4'.
+ *
+ * @package     tool_selfsignuphardlifecycle
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['col_accountoverridden'] = 'Períodos de cuenta anulados';
+$string['col_accountstatus'] = 'Estado de la cuenta';
+$string['col_auth'] = 'Autenticación';
+$string['col_nextstep'] = 'Paso siguiente';
+$string['col_profile'] = 'Perfil del usuario';
+$string['col_timecreated'] = 'Fecha de creación de cuenta';
+$string['eventuserdeleted'] = 'Usuario eliminado';
+$string['eventusersuspended'] = 'Usuario suspendido';
+$string['nextstep_deletioncomingup'] = 'Será eliminado en {$a->date}';
+$string['nextstep_suspensioncomingup'] = 'Será suspendido en {$a->date}';
+$string['nextstep_unknown'] = 'Desconocido';
+$string['profileedit'] = 'Editar';
+$string['profileview'] = 'Ver';
+$string['setting_authmethodsheading'] = 'Métodos de autenticación';
+$string['setting_cohortexceptions'] = 'Cohortes a ignorar';
+$string['setting_cohortexceptionsheading'] = 'Excepciones de cohortes';
+$string['setting_coveredauth'] = 'Métodos de autenticación cubiertos';
+$string['setting_enablecohortexceptions'] = 'Habilitar excepciones de cohortes';
+$string['setting_enablecohortexceptions_desc'] = 'Con esta configuración, usted puede definir excepciones de cohortes.';
+$string['setting_enableuseroverrides'] = 'Habilitar anulaciones de usuarios';
+$string['setting_enableusersuspension'] = 'Habilitar suspensión del usuario antes de eliminación';
+$string['setting_enableusersuspension_desc'] = 'Con esta configuración, usted puede configurar la herramienta para suspender usuarios antes de eliminarlos.';
+$string['setting_userdeletionheading'] = 'Eliminación de usuario';
+$string['setting_userdeletionoverridefield'] = 'Campo de perfil de anulación de eliminación de usuario';
+$string['setting_userdeletionperiod'] = 'Períodod de eliminación de usuario';
+$string['setting_useroverridesheading'] = 'Anulaciones de usuario';
+$string['setting_usersuspensionheading'] = 'Suspensión de usuario';
+$string['setting_usersuspensionoverridefield'] = 'CAmpo de perfil de anulación de suspensión de usuario';
+$string['setting_usersuspensionperiod'] = 'Período de suspensión de usuario';
+$string['settingsuserlist'] = 'Lista de usuarios';
+$string['status_active'] = 'Activo';
+$string['status_suspended'] = 'Suspendido';
+$string['status_unknown'] = 'Desconocido';
+$string['taskprocesslifecycle'] = 'Procesar ciclo de vida para usuarios auto-apuntados';
