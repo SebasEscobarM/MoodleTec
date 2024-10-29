2ba52c9216a4ec7362772ecfead2f19be87a780e
Sebastian Escobar Marin <sebastianescobar17@hotmail.com>
2024-10-29 12:07:41 -0500

    Revert "feat: added the project and docker"
    This reverts commit fcd2b33cc600785e1784eddf48561b29fb6197b0.

--- moodledata/cache/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-// ********** This file is generated DO NOT EDIT **********
-$CFG->siteidentifier = 'pzz4KFftqOZ5sDLYkDSOYzNtI7J3HTTklocalhost';
-$CFG->bootstraphash = '614faa44005c3a52048a8009613d9b26';
-// Only if the file is not stale and has not been defined.
-if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
-    define('SYSCONTEXTID', 1);
-}
