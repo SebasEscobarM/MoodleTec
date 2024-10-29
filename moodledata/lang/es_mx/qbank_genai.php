3aab9cdb50fa9d88e3bf4c098797204849d33fc2
Sebastian Escobar Marin <sebastianescobar17@hotmail.com>
2024-10-29 12:41:47 -0500

    added moodledata

---
+++ moodledata/lang/es_mx/qbank_genai.php
@@ -0,0 +1,38 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'qbank_genai', language 'es_mx', version '4.4'.
+ *
+ * @package     qbank_genai
+ * @category    string
+ * @copyright   1999 Martin Dougiamas and contributors
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['assistantid'] = 'ID del asistente';
+$string['assistantiddesc'] = 'ID concerniente al <a href="https://platform.openai.com/docs/assistants/overview" target="_blank">Assistants API</a> de OpenAI. esto será configurado por el plugin.';
+$string['errormsg_noneselected'] = 'Por favor seleccione al menos un recurso.';
+$string['noopenaiapikey'] = 'Usted aun no ha configurado al momento una clave de OpenAI API. Por favor revise las configuraciones del plugin.';
+$string['noresources'] = 'No hay recursos en su curso.';
+$string['ongoingtasks'] = 'Están ejecutándose los siguientes trabajos de generación:';
+$string['openaiapikey'] = 'Clave OpenAI API';
+$string['openaiapikeydesc'] = 'A ser creada en <a href="https://platform.openai.com/api-keys" target="_blank">https://platform.openai.com/api-keys</a>.';
+$string['pluginname'] = 'Banco de Preguntas de IA Generativa';
+$string['privacy:metadata'] = 'El plugin de Banco de Preguntas de IA Generativa no almacena ningún dato personal.';
+$string['title'] = 'Generar preguntas';
